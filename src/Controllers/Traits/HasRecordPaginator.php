d1b17ff659c3cdcf6b7f167529d0ae88b428e6d8
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-09-12 10:44:01 +0300

    update record paginator

--- src/Controllers/Traits/HasRecordPaginator.php
+++ src/Controllers/Traits/HasRecordPaginator.php
@@ -2,25 +2,51 @@
 
 namespace ByTIC\Common\Controllers\Traits;
 
+use Nip_Record_Paginator as RecordPaginator;
+
 trait HasRecordPaginator
 {
 
-    public $paginator = null;
+    /**
+     * @var null|RecordPaginator
+     */
+    protected $paginator = null;
 
-    public function getRPaginator()
+    public function getRecordPaginator()
     {
         if ($this->paginator === null) {
-            $this->initRPaginator();
+            $this->initRecordPaginator();
         }
         return $this->paginator;
     }
-    
-    public function initRPaginator()
+
+    public function initRecordPaginator()
     {
-        $this->paginator = $this->newRPaginator();
+        $this->setRecordPaginator($this->newRecordPaginator());
+        $this->prepareRecordPaginator();
     }
 
-    public function newRPaginator()
+    public function prepareRecordPaginator()
+    {
+        $this->getRecordPaginator()->setPage(intval($_GET['page']));
+        $this->getRecordPaginator()->setItemsPerPage(50);
+    }
+
+    /**
+     * @param RecordPaginator $paginator
+     * @return $this
+     */
+    public function setRecordPaginator($paginator)
+    {
+        $this->paginator = $paginator;
+
+        return $this;
+    }
+
+    /**
+     * @return RecordPaginator
+     */
+    public function newRecordPaginator()
     {
         return new \Nip_Record_Paginator();
     }
