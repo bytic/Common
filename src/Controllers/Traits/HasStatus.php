e65f58aa984cdaf45e9b85569f9d9f63ab51c25c
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-09-16 15:29:50 +0300

    update has Status controller trait

--- src/Controllers/Traits/HasStatus.php
+++ src/Controllers/Traits/HasStatus.php
@@ -19,20 +19,21 @@ trait HasStatus
 
     public function changeStatus()
     {
-        $this->item = $this->checkItem();
+        $item = $this->getModelFromRequest();
 
         $status = $_GET['status'];
+        $redirect = $_SERVER['HTTP_REFERER'];
         $availableStatuses = $this->getModelManager()->getStatusProperty('name');
         if (in_array($status, $availableStatuses)) {
             $this->item->setStatus($status);
-            $this->changeStatusRedirect($this->item);
+            $this->changeStatusRedirect($item);
         }
         $this->flashRedirect($this->getModelManager()->getMessage('status.invalid-status'), $redirect, 'error');
     }
 
     public function changeStatusRedirect($item)
     {
-        $redirect = $_SERVER['HTTP_REFERER'];
+        $redirect = $_SERVER['HTTP_REFERER'] ? $_SERVER['HTTP_REFERER'] : $item->getURL();
         $this->flashRedirect($this->getModelManager()->getMessage('status.success'), $redirect);
     }
 
