39b8473dd4c9370bdff87c9852aba8ca8edeb74d
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-09-17 20:05:09 +0300

    update generic status

--- src/Records/Statuses/Generic.php
+++ src/Records/Statuses/Generic.php
@@ -2,10 +2,15 @@
 
 namespace ByTIC\Common\Records\Statuses;
 
-use Nip\Records\RecordManager as Records;
+use ByTIC\Common\Records\Traits\I18n\RecordsTrait as RecordsTranslated;
 use Nip\Records\Record as Record;
+use Nip\Records\RecordManager as Records;
 use ReflectionClass;
 
+/**
+ * Class Generic
+ * @package ByTIC\Common\Records\Statuses
+ */
 abstract class Generic
 {
     public $name = null;
@@ -16,47 +21,43 @@ abstract class Generic
     /**
      * @var null|Record
      */
-    protected $_item;
+    protected $item;
 
     /**
      * @var null|Records
      */
-    protected $_manager;
-
-
-    public function setItem($Item)
-    {
-        $this->_item = $Item;
-        return $this;
-    }
-
-    public function getItem()
-    {
-        return $this->_item;
-    }
+    protected $manager;
 
     /**
-     * @return Records
+     * @return Records|RecordsTranslated
      */
     public function getManager()
     {
-        return $this->_manager;
+        return $this->manager;
     }
 
+    /**
+     * @param Records $manager
+     * @return $this
+     */
     public function setManager(Records $manager)
     {
-        $this->_manager = $manager;
+        $this->manager = $manager;
         $this->getName();
         $this->getLabel();
+
         return $this;
     }
 
-
+    /**
+     * @return string
+     */
     public function getName()
     {
         if ($this->name == null) {
             $this->initName();
         }
+
         return $this->name;
     }
 
@@ -65,67 +66,134 @@ abstract class Generic
         $this->name = $this->generateName();
     }
 
+    /**
+     * @return string
+     */
     public function generateName()
     {
         $name = (new ReflectionClass($this))->getShortName();
         $name = inflector()->unclassify($name);
+
         return $name;
     }
 
+    /**
+     * @param bool $short
+     * @return null
+     */
     public function getLabel($short = false)
     {
         if (!$this->label) {
-            $this->label = $this->getManager()->translate('statuses.' . $this->getName());
-            $this->label_short = $this->getManager()->translate('statuses.' . $this->getName() . '.short');
+            $this->label = $this->getManager()->translate('statuses.'.$this->getName());
+            $this->label_short = $this->getManager()->translate('statuses.'.$this->getName().'.short');
         }
+
         return $short ? $this->label_short : $this->label;
     }
 
+    /**
+     * @param bool $short
+     * @return string
+     */
     public function getLabelHTML($short = false)
     {
-        return '<span class="label label-'.$this->getColorClass().'" rel="tooltip" title="'.$this->getLabel().'"  style="'.$this->getColorCSS().'">
+        return '<span class="'.$this->getLabelClasses().'" rel="tooltip" title="'.$this->getLabel().'"  
+        style="'.$this->getColorCSS().'">
             '.$this->getLabel($short).'
         </span>';
     }
 
+    /**
+     * @return string
+     */
+    public function getLabelClasses()
+    {
+        return 'label label-'.$this->getColorClass();
+    }
+
+    /**
+     * @return string
+     */
     public function getColorClass()
     {
         return 'default';
     }
 
+    /**
+     * @return string
+     */
     public function getColorCSS()
     {
-        return 'background-color: '.$this->getBGColor().';color:'.$this->getFGColor().';';
+        $css = [];
+        if ($this->getBGColor()) {
+            $css[] = 'background-color: '.$this->getBGColor();
+        }
+        if ($this->getBGColor()) {
+            $css[] = 'color: '.$this->getFGColor();
+        }
+
+        return implode(';', $css);
     }
 
+    /**
+     * @return bool|string
+     */
     public function getBGColor()
     {
-        return '#999';
+        return false;
     }
 
+    /**
+     * @return bool|string
+     */
     public function getFGColor()
     {
-        return '#fff';
+        return false;
     }
 
+    /**
+     * @return bool|void
+     */
     public function update()
     {
         $item = $this->getItem();
         if ($item) {
+            /** @noinspection PhpUndefinedFieldInspection */
             $item->status = $this->getName();
             $this->preUpdate();
             $return = $item->saveRecord();
             $this->postUpdate();
+
             return $return;
         }
+
         return false;
     }
 
-    public function  preUpdate()
+    /**
+     * @return Record|null
+     */
+    public function getItem()
+    {
+        return $this->item;
+    }
+
+    /**
+     * @param $i
+     * @return $this
+     */
+    public function setItem($i)
+    {
+        $this->item = $i;
+
+        return $this;
+    }
+
+    public function preUpdate()
     {
     }
 
-    public function  postUpdate()
+    public function postUpdate()
     {
     }
 }
