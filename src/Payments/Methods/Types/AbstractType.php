bc3facf900c8c22fda281e79f294ed999cc98a53
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-09-30 18:27:57 +0300

    add payment types

---
+++ src/Payments/Methods/Types/AbstractType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ByTIC\Common\Payments\Methods\Types;
+
+use ByTIC\Common\Records\Types\Generic;
+
+/**
+ * Class AbstractType
+ * @package ByTIC\Common\Payments\Methods\Types
+ */
+abstract class AbstractType extends Generic
+{
+
+    protected $message;
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        if (!$this->message) {
+            $this->message = $this->getManager()->getMessage('types.' . $this->getName());
+        }
+        return $this->message;
+    }
+
+    public function afterInsert()
+    {
+    }
+
+    /**
+     * @return bool
+     */
+    public function checkConfirmRedirect()
+    {
+        return false;
+    }
+}
