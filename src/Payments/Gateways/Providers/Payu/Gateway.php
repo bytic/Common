e642f3071b834fc9e56a503ade216e2e356356e5
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-10-04 15:12:40 +0300

    finish payu ipn response and tests

--- src/Payments/Gateways/Providers/Payu/Gateway.php
+++ src/Payments/Gateways/Providers/Payu/Gateway.php
@@ -6,13 +6,12 @@ use ByTIC\Common\Payments\Gateways\Providers\AbstractGateway\Gateway as Abstract
 use ByTIC\Common\Payments\Gateways\Providers\Payu\Message\CompletePurchaseResponse;
 use ByTIC\Common\Payments\Gateways\Providers\Payu\Message\PurchaseRequest;
 use ByTIC\Common\Payments\Gateways\Providers\Payu\Message\PurchaseResponse;
-use ByTIC\Common\Payments\Gateways\Providers\Payu\Message\ServerCompleteResponse;
+use ByTIC\Common\Payments\Gateways\Providers\Payu\Message\ServerCompletePurchaseResponse;
 
 /**
  * Class Gateway
  * @package ByTIC\Common\Payments\Gateways\Providers\Payu
  *
- * @method Payu getProviderClass
  * @method PurchaseRequest purchaseFromModel($record)
  *
  */
@@ -39,7 +38,7 @@ class Gateway extends AbstractGateway
 
     /**
      * @param array $parameters
-     * @return ServerCompleteResponse
+     * @return ServerCompletePurchaseResponse
      */
     public function serverCompletePurchase(array $parameters = [])
     {
@@ -65,6 +64,26 @@ class Gateway extends AbstractGateway
     }
 
     /**
+     * @return bool
+     */
+    public function isActive()
+    {
+        if ($this->getMerchant() && $this->getSecretKey()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMerchant()
+    {
+        return $this->getParameter('merchant');
+    }
+
+    /**
      * @return mixed
      */
     public function getSecretKey()
@@ -72,90 +91,19 @@ class Gateway extends AbstractGateway
         return $this->getParameter('secretKey');
     }
 
-    /**
-     * @return bool
-     */
-    public function isActive()
-    {
-        if ($this->getOption('merchant') && $this->getOption('secretKey')) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public function detectIPNResponse()
-    {
-        return $this->detectRequestFields($_POST, ['HASH', 'REFNOEXT']);
-    }
-
-    public function parseIPNResponse()
-    {
+//    public function parseIPNResponse()
+//    {
+//        echo "Verified OK!";
 //
-//        $donation = Donations::instance()->findOne($_POST["REFNOEXT"]);
 //        if ($donation) {
-//            $method = $donation->getMethod();
-//            $this->setOptions($method->getOptions('payu'));
-//            $this->setPaymentMethodModel($method);
+//            $donation->received = date(DATE_DB);
+//            $donation->setStatus('active');
+//
+//            /* ePayment response */
+//            $result_hash = $gateway->hmac($return);
+//            echo "<EPAYMENT>" . $date_return . "|" . $result_hash . "</EPAYMENT>";
+//        } else {
+//            echo 'error donation';
 //        }
-
-        /* Internet Payment Notification */
-        $gateway = $this->getProviderClass();
-        $result = "";                /* string for compute HASH for received data */
-        $return = "";                /* string to compute HASH for return result */
-        $signature = $_POST["HASH"];    /* HASH received */
-        $body = "";
-
-
-
-        /* read info received */
-        ob_start();
-        while (list($key, $val) = each($_POST)) {
-            $$key = $val;
-
-            /* get values */
-            if ($key != "HASH") {
-
-                if (is_array($val)) {
-                    $result .= ArrayExpand($val);
-                } else {
-                    $size = strlen(StripSlashes($val));
-                    $result .= $size . StripSlashes($val);
-                }
-
-            }
-
-        }
-
-        $body = ob_get_contents();
-        ob_end_flush();
-
-
-        $date_return = date("YmdGis");
-
-        $return = strlen($_POST["IPN_PID"][0]) . $_POST["IPN_PID"][0] . strlen($_POST["IPN_PNAME"][0]) . $_POST["IPN_PNAME"][0];
-        $return .= strlen($_POST["IPN_DATE"]) . $_POST["IPN_DATE"] . strlen($date_return) . $date_return;
-
-        $hash = $gateway->hmac($result); /* HASH for data received */
-
-        $body .= $result . "\r\n\r\nHash: " . $hash . "\r\n\r\nSignature: " . $signature . "\r\n\r\nReturnSTR: " . $return;
-
-        if ($hash == $signature) {
-            echo "Verified OK!";
-
-            if ($donation) {
-                $donation->received = date(DATE_DB);
-                $donation->setStatus('active');
-
-                /* ePayment response */
-                $result_hash = $gateway->hmac($return);
-                echo "<EPAYMENT>" . $date_return . "|" . $result_hash . "</EPAYMENT>";
-            } else {
-                echo 'error donation';
-            }
-        } else {
-            /* warning email */
-            mail("webmaster@gecad.ro", "BAD IPN Signature", $body, "");
-        }
-    }
+//    }
 }
