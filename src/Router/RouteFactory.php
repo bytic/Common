1deb31b2d2ba8ce8cfa7921ce7eecfee78aea101
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-10-21 19:27:09 +0300

    initial add RouteFactory

---
+++ src/Router/RouteFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ByTIC\Common\Router;
+
+use Nip\Router\RouteCollection;
+use Nip\Router\RouteFactory as NipRouteFactory;
+
+/**
+ * Class RouteFactory
+ * @package ByTIC\Common\Router
+ */
+class RouteFactory extends NipRouteFactory
+{
+
+    /**
+     * @param RouteCollection $collection
+     * @param $module
+     * @param $prefix
+     */
+    public static function generateGenericModuleDefaultRoutes($collection, $module, $prefix)
+    {
+        $classBase = ucfirst($module).'_Route_';
+        self::generateIndexRoute($collection, $module, $classBase.'Literal', '/admin');
+        self::generateStandardRoute($collection, $module.".default", $classBase.'Standard', $prefix);
+    }
+
+    /**
+     * @param RouteCollection $collection
+     * @param $class
+     */
+    public static function generateModuleDefaultErrorRoutes($collection, $class)
+    {
+        self::generateLiteralRoute($collection, "default.error.403", $class, '', '/403',
+            ["controller" => "error", "action" => "index"]);
+
+        self::generateLiteralRoute($collection, "default.error.404", $class, '', '/404',
+            ["controller" => "error", "action" => "index"]);
+
+        self::generateLiteralRoute($collection, "default.error.500", $class, '', '/500',
+            ["controller" => "error", "action" => "index"]);
+    }
+}
