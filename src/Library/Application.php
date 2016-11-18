083545622a404ccf7fad765f45c84aaf26287470
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-11-18 11:33:34 +0200

    initial add application

--- src/Library/Application.php
+++ src/Library/Application.php
@@ -3,6 +3,8 @@
 namespace ByTIC\Common\Library;
 
 use Nip\Application as NipApplication;
+use Nip\Http\Response\Response;
+use Nip\Request;
 
 /**
  * Class Application
@@ -33,5 +35,51 @@ abstract class Application extends NipApplication
         );
 
         ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.LIBRARY_PATH);
+    }
+
+    public function setupURLConstants()
+    {
+        parent::setupURLConstants();
+        require_once(ROOT_PATH.'config.php');
+    }
+
+    public function initLanguages()
+    {
+        $stageConfig = $this->getStaging()->getStage()->getConfig();
+        $availableLanguages = explode(',', $stageConfig->get('LOCALE.languages'));
+        $availableLanguages = is_array($availableLanguages) ? $availableLanguages : ['ro'];
+
+        $translator = $this->getTranslator();
+
+        $backend = new \Nip\I18n\Translator\Backend\File();
+        $translator->setBackend($backend);
+
+        foreach ($availableLanguages as $language) {
+            $backend->addLanguage($language, LANGUAGES_PATH.$language.DS);
+        }
+
+        $translator->setDefaultLanguage($stageConfig->get('LOCALE.language_default'));
+    }
+
+    public function preHandleRequest()
+    {
+        parent::preHandleRequest();
+        register_shutdown_function('__shutdown');
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function filterResponse(Response $response, Request $request)
+    {
+        parent::filterResponse($response, $request);
+
+        if (!$request->isCLI() && $this->getStaging()->getStage()->inTesting()) {
+            $this->getDebugBar()->injectDebugBar($response);
+        }
+
+        return $response;
     }
 }
