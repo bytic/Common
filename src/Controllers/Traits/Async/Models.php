bd9df7985c1ce553fcafd381c6e6b789ce072660
Gabriel Solomon <hello@gabrielsolomon.ro>
2016-12-17 16:31:38 +0200

    update async models

--- src/Controllers/Traits/Async/Models.php
+++ src/Controllers/Traits/Async/Models.php
@@ -2,6 +2,9 @@
 
 namespace ByTIC\Common\Controllers\Traits\Async;
 
+use Nip\Database\Query\Select as SelectQuery;
+use Nip\Records\RecordManager;
+
 /**
  * Class Models
  * @package ByTIC\Common\Controllers\Traits\Async
@@ -27,5 +30,52 @@ trait Models
         }
 
         $this->Async()->sendMessage('Items reordered');
+    }
+
+    /**
+     * @return RecordManager
+     */
+    abstract protected function getModelManager();
+
+    public function nameAutocomplete()
+    {
+        $query = $this->generateNameAutocompleteQuery();
+        $items = $this->getModelManager()->findByQuery($query);
+
+        $return = [];
+        foreach ($items as $user) {
+            $array = $user->toArray();
+            $array['value'] = $user->getName();
+            $array['label'] = $user->getName();
+            $return[] = $array;
+        }
+
+        $this->setResponseValues($return);
+        $this->output();
+    }
+
+    /**
+     * @return SelectQuery
+     */
+    protected function generateNameAutocompleteQuery()
+    {
+        $query = $this->getModelManager()->paramsToQuery();
+        $names = explode(' ', $_GET['term']);
+        foreach ($names as $name) {
+            $condition = $this->generateNameAutocompleteQueryCondition($query, $name);
+            $query->where($condition);
+        }
+        return $query;
+    }
+
+    /**
+     * @param SelectQuery $query
+     * @param string $term
+     * @return mixed
+     */
+    protected function generateNameAutocompleteQueryCondition($query, $term)
+    {
+        $condition = $query->getCondition("name LIKE ? ", "%{$term}%");
+        return $condition;
     }
 }
