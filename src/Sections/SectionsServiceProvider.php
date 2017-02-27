6d476b1ec32be5c1679647d7bef5c7a51f1c3dc9
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-02-27 17:24:51 +0200

    add new sections service manager

---
+++ src/Sections/SectionsServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ByTIC\Common\Sections;
+
+use Nip\Container\ServiceProvider\AbstractSignatureServiceProvider;
+
+/**
+ * Class MailServiceProvider
+ * @package Nip\Staging
+ */
+class SectionsServiceProvider extends AbstractSignatureServiceProvider
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        $this->registerSections();
+    }
+
+    protected function registerSections()
+    {
+        $sections = new SectionsManager();
+        $this->getContainer()->singleton('sections', $sections);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function provides()
+    {
+        return ['sections'];
+    }
+}
