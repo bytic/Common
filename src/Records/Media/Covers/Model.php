398867b74e0350cd3b8019ce7f7357388db0806a
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-05-22 17:42:19 +0300

    formatting

--- src/Records/Media/Covers/Model.php
+++ src/Records/Media/Covers/Model.php
@@ -2,14 +2,21 @@
 
 namespace ByTIC\Common\Records\Media\Covers;
 
+/**
+ * Class Model
+ * @package ByTIC\Common\Records\Media\Covers
+ */
 class Model extends \ByTIC\Common\Records\Media\Images\Model
 {
     protected $_mediaType = 'covers';
 
+    /**
+     * @param string $name
+     */
     public function setName($name)
     {
         parent::setName($name);
-        $this->url = $this->_model->getCoverURL($this->_type, $this->name);
-        $this->path = $this->_model->getCoverPath($this->_type, $this->name);
+        $this->url = $this->getModel()->getCoverURL($this->_type, $this->name);
+        $this->path = $this->getModel()->getCoverPath($this->_type, $this->name);
     }
 }
