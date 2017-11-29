a9baf017e61c93fa346e9bc632e201653d3ec6bf
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-11-29 21:45:29 +0000

    Apply fixes from StyleCI

--- src/Payments/Gateways/Providers/Euplatesc/Gateway.php
+++ src/Payments/Gateways/Providers/Euplatesc/Gateway.php
@@ -3,23 +3,32 @@
 use ByTIC\Common\Payments\Gateways\Providers\AbstractGateway\Gateway as AbstractGateway;
 
 /** * Class Gateway * @package ByTIC\Common\Payments\Gateways\Providers\Euplatesc * */class Gateway extends AbstractGateway
-{    /**     * @param $value     * @return mixed     */    public function setMid($value)
-{
-    return $this->setParameter('mid', $value);
-}    /**     * @param $value     * @return mixed     */    public function setKey($value)
-{
-    return $this->setParameter('key', $value);
-}    /**     * @return bool     */    public function isActive()
-{
-    if ($this->getMid() && $this->getKey()) {
-        return true;
+{    /**     * @param $value     * @return mixed     */
+    public function setMid($value)
+    {
+        return $this->setParameter('mid', $value);
     }
-    return false;
-}    /**     * @return mixed     */    public function getMid()
-{
-    return $this->getParameter('mid');
-}    /**     * @return mixed     */    public function getKey()
-{
-    return $this->getParameter('key');
-}
+    /**         * @param $value         * @return mixed         */
+    public function setKey($value)
+    {
+        return $this->setParameter('key', $value);
+    }
+    /**         * @return bool         */
+    public function isActive()
+    {
+        if ($this->getMid() && $this->getKey()) {
+            return true;
+        }
+        return false;
+    }
+    /**         * @return mixed         */
+    public function getMid()
+    {
+        return $this->getParameter('mid');
+    }
+    /**         * @return mixed         */
+    public function getKey()
+    {
+        return $this->getParameter('key');
+    }
 }
