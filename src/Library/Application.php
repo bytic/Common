d8166f00cf3f37ce71764eb0212a914122f85cec
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-12-15 19:56:32 +0200

    add autoloader classmap and namespace methods

--- src/Library/Application.php
+++ src/Library/Application.php
@@ -2,7 +2,7 @@
 
 namespace ByTIC\Common\Library;
 
-use Nip\Application as NipApplication;
+use Nip\Application\Application as NipApplication;
 use Nip\Http\Response\Response;
 use Nip\Request;
 
@@ -15,28 +15,49 @@ abstract class Application extends NipApplication
     public function setupConfig()
     {
         parent::setupConfig();
-        app('config')->mergeFile(CONFIG_PATH.'general.ini');
+        app('config')->mergeFile(CONFIG_PATH . 'general.ini');
     }
 
     public function setupAutoLoaderCache()
     {
-        $this->getAutoLoader()->setCachePath(CACHE_PATH."autoloader".DS);
+        $this->getAutoLoader()->setCachePath(CACHE_PATH . "autoloader" . DS);
     }
 
     public function setupAutoLoaderPaths()
     {
-        $this->getAutoLoader()->addDirectory(APPLICATION_PATH);
-        $this->getAutoLoader()->addDirectory(LIBRARY_PATH);
+        $paths = $this->getAutoLoaderClassmapPaths();
+        foreach ($paths as $path) {
+            $this->getAutoLoader()->addDirectory($path);
+        }
 
-        $this->getAutoLoader()->addNamespace($this->getRootNamespace().'Models\\', APPLICATION_PATH.'models');
+        $namespaces = $this->getAutoLoaderNamespaces();
+        foreach ($namespaces as $namespace => $path) {
+            $this->getAutoLoader()->addNamespace($namespace, $path);
+        }
 
-        $this->getAutoLoader()->addNamespace($this->getRootNamespace().'Modules\Admin\\', MODULES_PATH.'admin');
-        $this->getAutoLoader()->addNamespace(
-            $this->getRootNamespace().'Modules\Frontend\\',
-            MODULES_PATH.'default'
-        );
+        ini_set("include_path", ini_get("include_path") . PATH_SEPARATOR . LIBRARY_PATH);
+    }
 
-        ini_set("include_path", ini_get("include_path").PATH_SEPARATOR.LIBRARY_PATH);
+    /**
+     * @return array
+     */
+    protected function getAutoLoaderClassmapPaths()
+    {
+        return [APPLICATION_PATH, LIBRARY_PATH];
+    }
+
+    /**
+     * @return array
+     */
+    protected function getAutoLoaderNamespaces()
+    {
+        return [
+            $this->getRootNamespace() . 'Models\\' => APPLICATION_PATH . 'models',
+            $this->getRootNamespace() . 'Modules\\' => MODULES_PATH,
+            $this->getRootNamespace() . 'Admin\\' => MODULES_PATH . 'admin',
+            $this->getRootNamespace() . 'Frontend\\' => MODULES_PATH . 'default',
+            $this->getRootNamespace() . 'Organizers\\' => MODULES_PATH . 'organizers',
+        ];
     }
 
     public function setupURLConstants()
@@ -62,7 +83,7 @@ abstract class Application extends NipApplication
         $translator->setBackend($backend);
 
         foreach ($availableLanguages as $language) {
-            $backend->addLanguage($language, LANGUAGES_PATH.$language.DS);
+            $backend->addLanguage($language, LANGUAGES_PATH . $language . DS);
         }
 
         $translator->setDefaultLanguage($stageConfig->get('LOCALE.language_default'));
