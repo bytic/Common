6ce02210683fb7fd4a1c9247f0aa08877dd579b4
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-06-13 15:32:39 +0300

    move currency to separate repo and deprecate

---
+++ legacy/Application/Models/Currencies/Traits/CurrencyRecordsTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ByTIC\Common\Application\Models\Currencies\Traits;
+
+/**
+ * Class CurrencyRecordsTrait
+ * @package ByTIC\Common\Application\Models\Currencies\Traits
+ *
+ * @deprecated use \ByTIC\Money\Models\Currencies\CurrenciesTrait;
+ */
+trait CurrencyRecordsTrait
+{
+    use \ByTIC\Money\Models\Currencies\CurrenciesTrait;
+}
---
+++ legacy/Application/Models/Currencies/Traits/HasCurrencyRecord.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ByTIC\Common\Application\Models\Currencies\Traits;
+
+/**
+ * Class HasCurrencyRecord
+ * @package ByTIC\Common\Application\Models\Currencies\Traits
+ *
+ * @deprecated use \ByTIC\Money\Models\Currencies\HasCurrencyRecord;
+ */
+trait HasCurrencyRecord
+{
+    use \ByTIC\Money\Models\Currencies\HasCurrencyRecord;
+}
