cb1890d2dba5945c09a27b5261ad1a5dc880b70b
Gabriel Solomon <hello@gabrielsolomon.ro>
2019-12-30 16:42:27 +0200

    move routerfactory to legacy

---
+++ legacy/Router/RouteFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ByTIC\Common\Router;
+
+use Nip\Router\RouteFactory as NipRouteFactory;
+
+/**
+ * Class RouteFactory
+ * @package ByTIC\Common\Router
+ *
+ * @deprecated use \Nip\Router\RouteFactory
+ */
+class RouteFactory extends NipRouteFactory
+{
+}
