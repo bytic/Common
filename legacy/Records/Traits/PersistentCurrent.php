b7fb3de66fd91cd5ad631e373de49d5c3a5711cb
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-11-20 15:57:22 +0200

    update persistant current trait

---
+++ legacy/Records/Traits/PersistentCurrent.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ByTIC\Common\Records\Traits;
+
+/**
+ * Class PersistentCurrent
+ * @deprecated use \ByTIC\PersistentData\PersistentManagerTrait;
+ */
+trait PersistentCurrent
+{
+    use \ByTIC\PersistentData\PersistentManagerTrait;
+}
