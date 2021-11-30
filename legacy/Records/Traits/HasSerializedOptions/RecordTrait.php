ba1def6cc0a82bd691f5a5bf4980a7bf78ca1016
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-11-30 10:33:42 +0200

    move serialized options to record behaiviors

---
+++ legacy/Records/Traits/HasSerializedOptions/RecordTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ByTIC\Common\Records\Traits\HasSerializedOptions;
+
+use ByTIC\Records\Behaviors\HasSerializedOptions\HasSerializedOptionsRecordTrait;
+
+/**
+ * Class RecordsTrait
+ * @package ByTIC\Common\Records\Traits\HasSerializedOptions
+ *
+ * @deprecated use \ByTIC\Records\Behaviors\HasSerializedOptions\HasSerializedOptionsRecordTrait
+ */
+trait RecordTrait
+{
+    use HasSerializedOptionsRecordTrait;
+}
